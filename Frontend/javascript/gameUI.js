772d80401fc8076d43ade35e21df372bb124bfac
lovyck <44643868+oblivionmasta@users.noreply.github.com>
2018-11-14 15:33:05 -0500

    COnverted the board to use div

--- Frontend/javascript/gameUI.js
+++ Frontend/javascript/gameUI.js
@@ -1,5 +1,21 @@
 window.onload = function(){
-	window.globalVar = true;
+	function createBoard(){
+		
+		var board = document.getElementById("boardGrid");
+		
+		for(var y = 7; y>-1;y--){
+			for(var x = 0; x<8;x++){
+				var square = document.createElement("div");
+				square.className = "boardSquareB";
+				square.setAttribute("onclick","displayID(this.id)");
+				square.setAttribute("onmouseover","makeIdAppear(this.id)");
+				square.id = x.toString()+y.toString();
+				board.appendChild(square);
+			}
+		}
+		
+	}
+	createBoard();
 };
 
 function displayID(id){
